b8803cbc478cde20754f97d6ad4c358bd545f620
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2023-05-23 22:24:20 -0300

    Testando variáveis

---
+++ Testes.java
@@ -0,0 +1,17 @@
+
+public class Testes {
+
+	public static void main(String[] args) {
+		
+		byte digito;
+		char sexo;
+		boolean confirmar;
+		String nome;
+		
+		digito = 2;
+		sexo = 'M';
+		confirmar = true;
+		nome = "Marcelo";
+	}
+
+}
