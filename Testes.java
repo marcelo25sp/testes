77625c472ebbdd4b1dcad27fb00cf6c72c4c8865
Marcelo Nascimento <marcelo.ngomes@hotmail.com>
2023-05-25 22:03:23 -0300

    Adicionando os primeiros comandos com a função JOptionPane

--- Testes.java
+++ Testes.java
@@ -1,23 +1,36 @@
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class Testes {
+
+	
 
 	public static void main(String[] args) {
 		
+		// inteiros
+		int matricula;
+		short AnoAtual;
+		long codigo;
 		byte digito;
+		
+		//reais
+		float nota;
+		double salario;
+		
+		//
 		char sexo;
+		
+		//lógicos
 		boolean confirmar;
+		
+		//caracteres
 		String nome;
 		
-		digito = 2;
-		sexo = 'M';
-		confirmar = true;
-		nome = "Marcelo";
 		
-		System.out.println("Digite o nome do usuário:");
-		Scanner teclado = new Scanner(System.in);
-		nome = teclado.next();
-		System.out.println("Bem vindo(a)" + nome);
+		sexo = JOptionPane.showInputDialog("Digite o sexo do usuário:[M/F]").charAt(0); 
+		matricula = Integer.parseInt(JOptionPane.showInputDialog("Digite o número da matrícula do usuário:"));
+		
 	}
 
 }
